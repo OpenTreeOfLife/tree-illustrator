76924f6629252faaccaf3f124e81eda712481f67
Jim Allman <jim@ibang.com>
2014-08-12 02:57:38 -0400

    Add support scripts and basic style switcher.

--- stylist/stylist.js
+++ stylist/stylist.js
@@ -1,3 +1,152 @@
+var availableStyles = [
+    {
+        name: "Basic", 
+        style:  { 
+          "width": 400,
+          "height": 200,
+          "padding": {"top": 10, "left": 30, "bottom": 30, "right": 10},
+          "data": [{"name": "table"}],
+          "scales": [
+            {
+              "name": "x", "type": "ordinal", "range": "width",
+              "domain": {"data": "table", "field": "data.x"}
+            },
+            {
+              "name": "y", "range": "height", "nice": true,
+              "domain": {"data": "table", "field": "data.y"}
+            }
+          ],
+          "axes": [
+            {"type": "x", "scale": "x"},
+            {"type": "y", "scale": "y"}
+          ],
+          "marks": [
+            {
+              "type": "rect",
+              "from": {"data": "table"},
+              "properties": {
+                "enter": {
+                  "x": {"scale": "x", "field": "data.x"},
+                  "y": {"scale": "y", "field": "data.y"},
+                  "y2": {"scale": "y", "value": 0},
+                  "width": {"scale": "x", "band": true, "offset": -1}
+                },
+                "update": {
+                  "fill": {"value": "steelblue"}
+                },
+                "hover": {
+                  "fill": {"value": "red"}
+                }
+              }
+            },
+            {
+              "type": "rect",
+              "interactive": false,
+              "from": {"data": "table"},
+              "properties": {
+                "enter": {
+                  "x": {"scale": "x", "field": "data.x", "offset": -3.5},
+                  "y": {"scale": "y", "field": "data.y", "offset": -3.5},
+                  "y2": {"scale": "y", "value": 0, "offset": 3.5},
+                  "width": {"scale": "x", "band": true, "offset": 6},
+                  "fill": {"value": "transparent"},
+                  "stroke": {"value": "red"},
+                  "strokeWidth": {"value": 2}
+                },
+                "update": {
+                  "strokeOpacity": {"value": 0}
+                },
+                "hover": {
+                  "strokeOpacity": {"value": 1}
+                }
+              }
+            }
+          ]
+        }
+    },
+    {
+        name: "Nature", 
+        style:  {
+          "width": 300,
+          "height": 500,
+          "padding": {"top": 80, "left": 30, "bottom": 30, "right": 10},
+          "data": [{"name": "table"}],
+          "scales": [
+            {
+              "name": "x", "type": "ordinal", "range": "width",
+              "domain": {"data": "table", "field": "data.x"}
+            },
+            {
+              "name": "y", "range": "height", "nice": true,
+              "domain": {"data": "table", "field": "data.y"}
+            }
+          ],
+          "axes": [
+            {"type": "x", "scale": "x"},
+            {"type": "y", "scale": "y"}
+          ],
+          "marks": [
+            {
+              "type": "rect",
+              "from": {"data": "table"},
+              "properties": {
+                "enter": {
+                  "x": {"scale": "x", "field": "data.x"},
+                  "y": {"scale": "y", "field": "data.y"},
+                  "y2": {"scale": "y", "value": 0},
+                  "width": {"scale": "x", "band": true, "offset": -1}
+                },
+                "update": {
+                  "fill": {"value": "navy"}
+                },
+                "hover": {
+                  "fill": {"value": "yellow"}
+                }
+              }
+            },
+            {
+              "type": "rect",
+              "interactive": false,
+              "from": {"data": "table"},
+              "properties": {
+                "enter": {
+                  "x": {"scale": "x", "field": "data.x", "offset": -3.5},
+                  "y": {"scale": "y", "field": "data.y", "offset": -3.5},
+                  "y2": {"scale": "y", "value": 0, "offset": 3.5},
+                  "width": {"scale": "x", "band": true, "offset": 6},
+                  "fill": {"value": "transparent"},
+                  "stroke": {"value": "red"},
+                  "strokeWidth": {"value": 2}
+                },
+                "update": {
+                  "strokeOpacity": {"value": 0}
+                },
+                "hover": {
+                  "strokeOpacity": {"value": 1}
+                }
+              }
+            }
+          ]
+        }
+    },
+    {
+        name: "Systematic Biology", 
+        style:  { }
+    },
+    {
+        name: "Nature (monochrome)", 
+        style:  { }
+    },
+    {
+        name: "Modern (from SysBio)", 
+        style:  { }
+    },
+    {
+        name: "Jim\'s favorites", 
+        style:  { }
+    }
+];
+
 var spec = {
   "width": 400,
   "height": 200,
@@ -74,9 +223,9 @@ var data = {table: [
   {"x": 19, "y": 49}, {"x": 20, "y": 75}
 ]};
 
-
 function refreshViz() {
-    vg.parse.spec(spec, function(chart) {
+    if (!viewModel.spec) return;
+    vg.parse.spec(viewModel.spec, function(chart) {
       var view = chart({el:"#view", data:data})
         .on("mouseover", function(event, item) {
           // invoke hover properties on cousin one hop forward in scenegraph
@@ -97,4 +246,38 @@ function refreshViz() {
         .update();
     });
 }
-refreshViz();
+
+var viewModel;
+$(document).ready(function() {
+    // create the viewModel (complete spec?) and build a matching UI
+    viewModel = {
+        spec: availableStyles[0].style,  // see above
+        data: data,  // see above
+        illustrationID: null,  // TODO: assign a key/ID when saved?
+        illustrationName: "Untitled"
+    };
+    var editorArea = $('#editor')[0];
+    ko.applyBindings(viewModel, editorArea);
+
+    // TODO: Add "safety net" if there are unsaved changes
+    // TODO: Add JSON support for older IE?
+    // TODO: Add bootstrap for style+behavior?
+
+    refreshViz();
+});
+
+function useChosenStyle() {
+    //var styleName = $('#style-chooser option:selected').val();
+    var styleName = $('#style-chooser').val();
+    var selectedStyles = $.grep(availableStyles, function(o) {return o.name === styleName;});
+    var styleInfo = null;
+    if (selectedStyles.length > 0) {
+        styleInfo = selectedStyles[0];
+    }
+    if (!styleName || !styleInfo) {
+        console.warn("No style found under '"+ styleName +"'!");
+        return;
+    }
+    viewModel.spec = styleInfo.style;
+    refreshViz();
+}
