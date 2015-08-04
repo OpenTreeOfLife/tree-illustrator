dc4067b009bbb5a291f0969d88b68a0cb2aaad20
Jim Allman <jim@ibang.com>
2015-08-04 11:25:09 -0400

    Add listener and cross-document messaging support.
    This is intended to support embedding one or more Tree Illustrator
    instances in an IPython notebook.

---
+++ stylist/ipython-notebook-bridge.js
@@ -0,0 +1,44 @@
+/*
+ * This script adds cross-document messaging (postMessage) support, to allow
+ * embedding one or many instances of the Tree Illustrator UI in an
+ * IPython notebook. This lets us provide tree data from the surrounding
+ * IPython session, and save SVG output or complete illustration JSON from each
+ * instance.
+ *
+ * This should be useful in pre-publication and exploratory scenarios for a
+ * single user, or fairly easy collaboration, using Wakari or another notebook
+ * server.
+ */
+
+// TODO: set expected calling domain when instance is created?
+
+function receiveMessage(msg) {
+    /* REMINDER: A message has three properties of interest:
+     *   msg.origin is the domain that holds the surrounding IPython notebook
+     *   msg.data is its payload
+     *   msg.source is the window that sent the message (the notebook viewer)
+     */
+    if (msg.source !== window.opener) {
+        alert("Expected this message from my window.opener!");
+        //return;
+    }
+
+    if (!msg.data['method']) {
+        alert("Expected a named method in msg.data.method!");
+        return;
+    }
+
+    switch(msg.data['method']) {
+        case '':
+            break;
+        default:
+            alert("Unxpected method ["+ msg.data.method +"] in this message!");
+            return;
+    }
+
+    // send a reply to the calling window
+    msg.source.postMessage("HI MOM", msg.origin);
+}
+
+window.addEventListener("message", receiveMessage, false);
+
