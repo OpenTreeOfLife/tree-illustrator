a171afd3fb09186df636eb9fd3021a81e9d0a8da
Jim Allman <jim@ibang.com>
2015-08-04 20:53:41 -0400

    Revert to trusty alert box.

--- stylist/ipynb-tree-illustrator.js
+++ stylist/ipynb-tree-illustrator.js
@@ -47,9 +47,10 @@ var IPythonTreeIllustrator = function(window, document, $) {
         }
 
         if (!cell || !('append_output' in cell)) {
-            throw new Error("Missing notebook cell as first argument! Try using 'this':"
+            alert("Missing notebook cell as first argument! Try 'this':"
                 + "\n  var ti = new IPythonTreeIllustrator.IllustratorWidget(this);"
                 + "\nREMINDER: Tree Illustrator doesn't currently work in static notebook views!");
+            return null;
         }
 
         if (!data || typeof(data) !== 'object') {
