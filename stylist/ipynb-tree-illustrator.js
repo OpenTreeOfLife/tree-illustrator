3baee394a862d9be1cab71fbd701c36ffd3993ff
Jim Allman <jim@ibang.com>
2015-11-18 11:20:24 -0500

    Test embedding options, esp. in IPython modal.

--- stylist/ipynb-tree-illustrator.js
+++ stylist/ipynb-tree-illustrator.js
@@ -1,19 +1,30 @@
 /*
  * This script can be imported into any IPython notebook (assumes the web UI)
- * to allow embedding the Tree Illustrator webapp in an IFRAME. This makes it a
- * static widget in IPython parlance, so it will be usable even in a "headless"
- * notebook viewer.
- *
+ * to allow embedding the Tree Illustrator webapp, where it can read data from
+ * the surrounding notebook and save results there. 
+ * 
  * This should be useful in pre-publication and exploratory scenarios for a
  * single user, or fairly easy collaboration using Wakari or another notebook
  * server.
  * 
- * Sensible encapsulation will let us embed one or many instances of the Tree
- * Illustrator UI in a notebook. 
+ * There are a few options for where the app would appear:
+ *
+ *   1. in a modal popup (in an IFRAME)
+ *      This works best in a live (vs static HTML) notebook, where we an add a
+ *      toolbar button and use IPython's built-in modal.
+ *
+ *   2. "inline" in a notebook cell (in an IFRAME)
+ *      This makes a "static widget" in IPython parlance, so it should work
+ *      even in a static HTML notebook. This allows multiple instances of Tree
+ *      Illustrator to exist in different cells; is that useful?
+ *
+ *   3. in a new browser tab or window (esp. for static notebooks)
+ *      This would need an additional bridge from the calling window to the new
+ *      one. On the plus side, it should work in a live *or* static notebook.
  * 
- * We'll use cross-document messaging (postMessage) to provide tree data from
- * the surrounding IPython session and to save SVG output (or complete
- * illustration JSON) from the Tree Illustrator. [1,2]
+ * In any case, we'll use cross-document messaging (postMessage) to provide
+ * tree data from the surrounding IPython session and to save SVG output (or
+ * complete illustration JSON) from the Tree Illustrator. [1,2]
  *
  * To use this, add a code cell to your IPython notebook and embed this script
  *   %%javascript
@@ -27,10 +38,17 @@
 
 var IPythonTreeIllustrator = function(window, document, $) {
 
-    // keep track of all active instances (widgets), keyed by element ID
+    // Are we running in a "live" notebook, or static HTML?
+    var isLiveNotebook = $('body.notebook_app').length > 0;
+    var isStaticNotebook = !(isLiveNotebook);
+
+    // Define some enumerated values for callers.
+    var SINGLETON = 'SINGLETON';
+
+    // Keep track of all active instances (widgets), keyed by element ID
     var widgets = { };
 
-    // assign unique, serial element IDs
+    // Assign unique, serial element IDs
     var nextAvailableWidgetID = 0;
 
     var getNextAvailableWidgetID = function( ) {
@@ -40,41 +58,103 @@ var IPythonTreeIllustrator = function(window, document, $) {
         return ('tree-illustrator-'+ readyID);
     } 
 
-    var IllustratorWidget = function(cell, data) {
+    var IllustratorWidget = function(target, data) {
         if ( !(this instanceof IllustratorWidget) ) {
             console.warn("MISSING 'new' keyword for IllustratorWidget, patching this now");
             return new IllustratorWidget(data);
         }
 
-        if (!cell || !('append_output' in cell)) {
-            alert("Missing notebook cell as first argument! Try 'this':"
-                + "\n  var ti = new IPythonTreeIllustrator.IllustratorWidget(this);"
-                + "\nREMINDER: Tree Illustrator doesn't currently work in static notebook views!");
-            return null;
-        }
+        // Safely refer to this instance below
+        var self = this;
 
         if (!data || typeof(data) !== 'object') {
             // instance will load the "empty" illustration as usual?
+            console.log("No data specified for Tree Illustrator, will load placeholder");
         }
 
-        // safely refer to this instance
-        var self = this;
+        if (target === SINGLETON) {
+            if (isLiveNotebook) {
+                // Use the modal popup support in IPython
+                var elementID = getNextAvailableWidgetID();
+                showInModalPopup(elementID, data);
+            } else {  // it's a static HTML notebok
+                // Use a new browser window or tab
+                self.showInNewWindow(elementID, data);
+            }
+        } else {
+            // try to embed in the specified cell
+            if (!cell || !('append_output' in cell)) {
+                if (isLiveNotebook) {
+                    alert("Missing notebook cell as first argument! Try 'this':"
+                        + "\n  var ti = new IPythonTreeIllustrator.IllustratorWidget(this);");
+                } else {
+                    alert("REMINDER: Tree Illustrator can't be embedded in a cell in the static HTML notebook!");
+                }
+                return null;
+            }
+            self.showInNotebookCell(cell);
+        }
 
-        /* define PRIVATE members (variables and methods)functions and with 'var' */
+        /* define PRIVATE members (variables and functions ) with 'var' */
+
+        var getIframeMarkup = function() {
+            // TODO: add version/SHA argument here?
+            return '<iframe id="'+ elementID +'" width="100%" height="500" \
+                            src="http://rawgit.com/OpenTreeOfLife/tree-illustrator/master/stylist/stylist.html" \
+                            frameborder="0" allowfullscreen="allowfullscreen"> \
+                    </iframe>';
+        }
+
+        var showInNewWindow = function() {
+            // TODO: Show the Tree Illustrator in a new browser window or tab, with a link
+            // back to the calling window.
+            alert('showInNewWindow(): COMING SOON');
+        }
+
+        var showInNotebookCell = function(cell) {
+            // create my IFRAME element in the output of the current notebook cell
+            var elementID = getNextAvailableWidgetID();
+            // N.B. This ID is mostly for internal use; user probably calls this something else
+            cell.append_display_data({
+              'data': {
+                'text/html': self.getIframeMarkup()
+              } 
+            })
+        }
+
+        var showInModalPopup = function(elementID, data) {
+            // Use IPython's support for a single modal popup, adapted from
+            // https://github.com/minrk/ipython_extensions/blob/70ed77bd7fd36fbead09a1df41f93cab5cfdfe92/nbextensions/gist.js
+            IPython.dialog.modal({
+                title: "GitHub OAuth",
+                body: dialog,
+                buttons : {
+                    "Cancel": {},
+                    "OK": {
+                        class: "btn-primary",
+                        click: function () {
+                            var token = $(this).find('input').val();
+                            localStorage[token_name] = token;
+                            gist_notebook();
+                        }
+                    }
+                },
+                open : function (event, ui) {
+                    var that = $(this);
+                    // Upon ENTER, click the OK button.
+                    that.find('input[type="text"]').keydown(function (event, ui) {
+                        if (event.which === 13) {
+                            that.find('.btn-primary').first().click();
+                            return false;
+                        }
+                    });
+                    that.find('input[type="text"]').focus().select();
+                }
+            });
+        };
 
         /* define PUBLIC variables (and privileged methods) with 'self' */
 
-        // create my IFRAME element in the output of the current notebook cell
-        var elementID = getNextAvailableWidgetID();
-        // N.B. This ID is mostly for internal use; user probably calls this something else
-        cell.append_display_data({
-          'data': {
-            'text/html': '<iframe id="'+ elementID +'" width="100%" height="500" \
-                           src="http://rawgit.com/OpenTreeOfLife/tree-illustrator/master/stylist/stylist.html" \
-                           frameborder="0" allowfullscreen="allowfullscreen"> \
-                          </iframe>'
-          } 
-        })
         var elementSelector = ('#'+ elementID);
         self.ti_element = $(elementSelector)[0];
         self.ti_window = self.ti_element.contentWindow;
@@ -82,21 +162,34 @@ var IPythonTreeIllustrator = function(window, document, $) {
         // add this instance to the registry above
         widgets[elementID] = self;
     }
+
     /* define PUBLIC methods (that don't need private data) in its prototype */
     IllustratorWidget.prototype = {
         constructor: IllustratorWidget,
 
         addTree: function(data) {
-            // TODO
             var self = this;
+            // TODO
+        },
+        dumpCurrentIllustration: function(data) {
+            var self = this;
+            // TODO
+        },
+        dumpCurrentIllustrationSVG: function(data) {
+            var self = this;
+            // TODO
         }
+
     }
 
     /* expose class constructors (and static methods) for instantiation */
     return {
-        // TODO: expose enumerations?
+        // expose enumerations
+        SINGLETON: SINGLETON,
 
-        // TODO: expose static methods?
+        // expose static properties and methods
+        isLiveNotebook: isLiveNotebook,
+        isStaticNotebook: isStaticNotebook,
 
         // expose available classes
         IllustratorWidget: IllustratorWidget
