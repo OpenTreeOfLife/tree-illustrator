ed5e2b4cbd682dcae068efc69433af0cf7e931f1
Jim Allman <jim@ibang.com>
2016-03-03 21:52:07 -0500

    Remove (inactive) start- and end-angle handles.
    These properties of radial trees are not currently being applied!  I'll
    restore the handles once the manual-entry (sidecar) UI is doing the
    right thing with these.

--- stylist/src/vg.data.phylogram.js
+++ stylist/src/vg.data.phylogram.js
@@ -435,10 +435,13 @@ prototype.buildPhylogram = function(data) {
                   midPoint = cartesianToPolarProjection( midPoint, {returnType: 'POLAR_COORDS'} );
 
                   // pass all polar properties (angle, radius, theta) plus a descriptive name
+                  /* TODO: Restore these handles once we have their properties working correctly!
+                   *
                   handles.push( $.extend(startPoint, {name: 'start-angle',
                                                       tooltip: "Drag to change radius and starting angle"}) );
                   handles.push( $.extend(endPoint, {name: 'end-angle',
                                                     tooltip: "Drag to change radius and ending angle"}) );
+                  */
                   handles.push( $.extend(midPoint, {name: 'radius',
                                                     tooltip: "Drag to change this tree's radius" }) );
                   break;
