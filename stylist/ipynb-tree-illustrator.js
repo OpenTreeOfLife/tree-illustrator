a00e07f3ac42f7a5fe5a62c706093c0073f2648c
Jim Allman <jim@ibang.com>
2016-03-26 12:57:33 -0400

    Handle proper JSON from R kernel

--- stylist/ipynb-tree-illustrator.js
+++ stylist/ipynb-tree-illustrator.js
@@ -666,14 +666,17 @@ function listAllNotebookVars( callback ) {
                 console.log( out.content.data );
                 var restoredOutput;
                 try {
-                    // string should evaluate as JS (not valid JSON)
+                    // string should evaluate as JS (if not valid JSON)
                     // TODO: Confirm this in Jupyter docs!
                     switch (out.msg_type) {
                         case 'execute_result':  
                         case 'display_data':
                             // result should be in `data['text/plain']`
-                            //restoredOutput = JSON.parse(out.content.data['text/plain']);
-                            restoredOutput = eval(out.content.data['text/plain']);
+                            try {
+                                restoredOutput = JSON.parse(out.content.data['text/plain']);
+                            } catch (e) {
+                                restoredOutput = eval(out.content.data['text/plain']);
+                            }
                             break;
                         case 'stream':          
                             // result should be the main `data` property
@@ -763,15 +766,18 @@ function getNotebookVar( varName, callback ) {
                 console.log( out.content.data );
                 var restoredOutput;
                 try {
-                    // string should evaluate as JS (not valid JSON)
+                    // string should evaluate as JS (if not valid JSON)
                     // TODO: Confirm this in Jupyter docs!
                     switch (out.msg_type) {
                         case 'execute_result':  
                         case 'display_data':
                             debugger;
                             // result should be in `data['text/plain']`
-                            //restoredOutput = JSON.parse(out.content.data['text/plain']);
-                            restoredOutput = eval(out.content.data['text/plain']);
+                            try {
+                                restoredOutput = JSON.parse(out.content.data['text/plain']);
+                            } catch (e) {
+                                restoredOutput = eval(out.content.data['text/plain']);
+                            }
                             break;
                         case 'stream':          
                             // result should be the main `data` property
